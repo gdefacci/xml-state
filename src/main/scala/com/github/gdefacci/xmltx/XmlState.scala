3b29e52fd24eab621c1fe869c2c6ffad7430eed8
gdefacci <gdefacci@yahoo.it>
2017-01-13 18:36:30 +0100

    add select

--- src/main/scala/com/github/gdefacci/xmltx/XmlState.scala
+++ src/main/scala/com/github/gdefacci/xmltx/XmlState.scala
@@ -1,123 +1,159 @@
 package com.github.gdefacci.xmlstate
 
-class State[S,O](val run:S => (S,O)) {
-  
-  def map[B](f1:O => B):State[S,B] = 
+class State[S, O](val run: S => (S, O)) {
+
+  def map[B](f1: O => B): State[S, B] =
     new State(run.andThen { case (state, out) => state -> f1(out) })
-  
-  def flatMap[B](f1:O => State[S,B]):State[S,B] = 
-    new State[S,B]( run.andThen { case (state, out) => f1(out).run(state) } )
-  
+
+  def flatMap[B](f1: O => State[S, B]): State[S, B] =
+    new State[S, B](run.andThen { case (state, out) => f1(out).run(state) })
+
+}
+
+object XmlPredicate {
+  def apply(p: xml.Elem => Boolean) = new XmlPredicate {
+    def apply(el: xml.Elem): Boolean = p(el)
+  }
+}
+
+trait XmlPredicate extends (xml.Elem => Boolean) { self =>
+
+  def and(p: xml.Elem => Boolean) = XmlPredicate(el => self.apply(el) && p.apply(el))
+
+  def or(p: xml.Elem => Boolean) = XmlPredicate(el => self.apply(el) || p.apply(el))
+
+  def not = XmlPredicate(el => !self.apply(el))
 }
 
 object XmlState extends App {
-  
-  def read[T](f:xml.Elem => T) = new State[xml.Elem, T]( s => s -> f(s))
-  def write(s1: => xml.Elem) = new State[xml.Elem, Unit]( s => (s1, ():Unit) )
-  def current = new State[xml.Elem, xml.Elem]( s => (s, s) )
-  
-  def attribute(name:String) = read[Option[String]] { nd =>
+
+  case class ByName(name: String) extends XmlPredicate {
+    def apply(el: xml.Elem) = el.label == name
+  }
+
+  case class ByAttribute(name: String, predicate: String => Boolean) extends XmlPredicate {
+    def apply(el: xml.Elem) = el.attribute(name).map(el => predicate(el.text)).getOrElse(false)
+  }
+
+  def read[T](f: xml.Elem => T) = new State[xml.Elem, T](s => s -> f(s))
+  def write(s1: => xml.Elem) = new State[xml.Elem, Unit](s => (s1, (): Unit))
+  def current = new State[xml.Elem, xml.Elem](s => (s, s))
+
+  def attribute(name: String) = read[Option[String]] { nd =>
     nd.attribute(name).map(_.text)
   }
-  
-  def children(name:String) = read[Seq[xml.Elem]] { nd =>
+
+  def childElements(predicate: xml.Elem => Boolean) = read[Seq[xml.Elem]] { nd =>
     nd.child.collect {
-      case el:xml.Elem if el.label == name => el
+      case el: xml.Elem if predicate(el) => el
     }
   }
-  
-  def childOpt(name:String) = children(name).map(_.headOption)
-  def child(name:String) = children(name).map(_.head)
-  
-  def setName(nm:String) = for {
+  def childElementOpt(predicate: xml.Elem => Boolean): State[xml.Elem, Option[xml.Elem]] = childElements(predicate).map(_.headOption)
+  def childElement(predicate: xml.Elem => Boolean): State[xml.Elem, xml.Elem] = childElements(predicate).map(_.head)
+
+  def children(name: String) = childElements(_.label == name)
+  def childOpt(name: String) = children(name).map(_.headOption)
+  def child(name: String) = children(name).map(_.head)
+
+  def setName(nm: String) = for {
     curr <- current
-    _ <- write( curr.copy(label = nm) )
+    _ <- write(curr.copy(label = nm))
   } yield ()
-  
-  def setAttr(nm:String, v:String) = for {
+
+  def setAttr(nm: String, v: String) = for {
     curr <- current
-    _ <- write( curr % xml.Attribute(None, nm, xml.Text(v), xml.Null) )
+    _ <- write(curr % xml.Attribute(None, nm, xml.Text(v), xml.Null))
   } yield ()
-  
-  def replace(el:xml.Elem, elem:xml.NodeSeq) = for {
+
+  def replace(el: xml.Elem, elem: xml.NodeSeq) = for {
     curr <- current
     _ <- write {
-      curr.copy( child = curr.child.flatMap {
-        case ch:xml.Elem if (ch == el) => elem
-        case x => xml.NodeSeq.fromSeq(x) 
-      } )
+      curr.copy(child = curr.child.flatMap {
+        case ch: xml.Elem if (ch == el) => elem
+        case x => xml.NodeSeq.fromSeq(x)
+      })
     }
   } yield ()
-  
-  def insertAfter(el:xml.Elem, newl:xml.NodeSeq) = 
-    replace(el, xml.NodeSeq.fromSeq(Seq(el)) ++  newl )
-  
-  def insertBefore(el:xml.Elem, newl:xml.Node) = 
-    replace(el, newl ++ xml.NodeSeq.fromSeq(Seq(el)) )
-  
-  def updateAttr(nm:String, update:String => String) = for {
+
+  def insertAfter(el: xml.Elem, newl: xml.NodeSeq) =
+    replace(el, xml.NodeSeq.fromSeq(Seq(el)) ++ newl)
+
+  def insertBefore(el: xml.Elem, newl: xml.Node) =
+    replace(el, newl ++ xml.NodeSeq.fromSeq(Seq(el)))
+
+  def updateAttr(nm: String, update: String => String) = for {
     curr <- current
     attr <- attribute(nm)
     _ <- attr match {
       case None => current
-      case Some(v) => write( curr % xml.Attribute(None, nm, xml.Text(update(v)), xml.Null) )
+      case Some(v) => write(curr % xml.Attribute(None, nm, xml.Text(update(v)), xml.Null))
     }
   } yield ()
-  
+
   def appendChild(ch: xml.NodeSeq) = for {
     curr <- current
-    _ <- write( curr.copy( child = curr.child ++ ch) )
-  } yield () 
-  
-  def updateAll(pred:xml.Elem => Boolean, st:State[xml.Elem,Unit]) = for {
-    curr <- current
-    r <- write {
-      val partChs:Seq[Either[xml.Node, xml.Elem]] = curr.child.collect { 
-        case ch:xml.Elem if pred(ch) => Right(ch) 
-        case ch => Left(ch) 
-      }
-      val newChild = partChs.map {
-        case Left(ch) => ch
-        case Right(ch) => st.run(ch)._1
-      }
-      curr.copy(child = newChild)
-    }
-  } yield r
-  
-  def updateChildren(elemName:String, st:State[xml.Elem,Unit]) = updateAll( el => el.label == elemName, st)
+    _ <- write(curr.copy(child = curr.child ++ ch))
+  } yield ()
 
-  def updateFirst(pred:xml.Elem => Boolean, st:State[xml.Elem,Unit]) = for {
-    curr <- current
-    r <- write {
-      var found = false
-      val partChs:Seq[Either[xml.Node, xml.Elem]] = curr.child.collect { 
-        case ch:xml.Elem if !found && pred(ch) => 
-          found = true
-          Right(ch) 
-        case ch => 
-          Left(ch) 
+  def select[R](pred: xml.Elem => Boolean, st: State[xml.Elem, R]) =
+    new State[xml.Elem, Seq[R]]({ curr =>
+      val partChs: Seq[Either[xml.Node, (xml.Elem, R)]] = curr.child.collect {
+        case ch: xml.Elem if pred(ch) => Right(st.run(ch))
+        case ch => Left(ch)
       }
       val newChild = partChs.map {
         case Left(ch) => ch
-        case Right(ch) => st.run(ch)._1
+        case Right(ch) => ch._1
       }
-      curr.copy(child = newChild)
+      val res = partChs.collect {
+        case Right((_, r)) => r
+      }
+      curr.copy(child = newChild) -> res
+    })
+
+  def selectPath[R](predicates: Seq[xml.Elem => Boolean], st: State[xml.Elem, R]): State[xml.Elem, Seq[R]] = {
+    predicates match {
+      case Seq() => st.map(el => Seq(el))
+      case predicate :: predicates => select[Seq[R]](predicate, selectPath(predicates, st)).map(_.flatten)
     }
-  } yield r
-  
-  def updateChild(elemName:String, st:State[xml.Elem,Unit]) = updateFirst( el => el.label == elemName, st)
+  }
+
+  def path[R](pth: Seq[String], st: State[xml.Elem, R]): State[xml.Elem, Seq[R]] = {
+    selectPath[R](pth.map(nm => (el: xml.Elem) => el.label == nm), st)
+  }
+
+  def selectChildren[R](elemName: String, st: State[xml.Elem, R]) = select(el => el.label == elemName, st)
+
+  def first[R](pred: xml.Elem => Boolean, st: State[xml.Elem, R]) = new State[xml.Elem, Option[R]]({ curr =>
+    val zsq = Seq.empty[Either[xml.Node, (xml.Elem, R)]]
+    val partChs = curr.child.foldLeft(zsq -> false) {
+      case ((acc, found), ch: xml.Elem) if !found && pred(ch) =>
+        (acc :+ Right(st.run(ch))) -> true
+      case ((acc, found), ch) =>
+        (acc :+ Left(ch)) -> found
+    }._1
+    val newChild = partChs.map {
+      case Left(ch) => ch
+      case Right(ch) => ch._1
+    }
+    val r = partChs.collectFirst {
+      case Right((_, r)) => r
+    }
+    curr.copy(child = newChild) -> r
+  })
+
+  def selectChild[R](elemName: String, st: State[xml.Elem, R]) = first(el => el.label == elemName, st)
 
   def text = read[String] { el => el.text }
-  def setText(v:String) = for {
+  def setText(v: String) = for {
     curr <- current
-    _ <- write { curr.copy( child = xml.Text(v) ) }
+    _ <- write { curr.copy(child = xml.Text(v)) }
   } yield ()
-  
-  def updateText(f:String => String) = for {
+
+  def updateText(f: String => String) = for {
     curr <- current
     txt <- text
-    _ <- write { curr.copy( child = xml.Text(f(txt)) ) }
+    _ <- write { curr.copy(child = xml.Text(f(txt))) }
   } yield ()
-  
-  
+
 }
--- src/test/scala/com/github/gdefacci/xmlstate/Test1.scala
+++ src/test/scala/com/github/gdefacci/xmlstate/Test1.scala
@@ -3,19 +3,20 @@ package com.github.gdefacci.xmlstate
 import org.scalatest.FunSuite
 
 import XmlState._
+import java.io.InputStreamReader
 
 class Test1 extends FunSuite {
 
   test("sample") {
     
     val tx1 = for {
-      _ <- updateChildren("pippo", for {
+      _ <- selectChildren("pippo", for {
         _ <- setAttr("a", "b")
         _ <- appendChild(<roo>yehh</roo>)
         age <- child("age")
         _ <- insertBefore(age, <street>pipppo</street>)
         _ <- insertAfter(age, <city>new york</city>)
-        _ <- updateChild("language", for {
+        _ <- selectChild("language", for {
           _ <- setText("English")
           _ <- updateAttr("level", "very " + _)
         } yield ())
@@ -37,4 +38,42 @@ class Test1 extends FunSuite {
 
   }
 
+  def load(pth:String) = xml.XML.load(Thread.currentThread().getContextClassLoader.getResourceAsStream(pth))
+  
+  test("planets") {
+    
+    val tx = for {
+      satellites <- path("planets" :: "planet" :: "satellites" :: "satellite" :: "name" :: Nil, text)
+    } yield satellites 
+    
+    assert( tx.run(load("planets.xml"))._2.toSet == Set("moon", "phobos", "deimos", "iii ganymede", "iv callisto", "titan", "rhea", "ariel", "miranda", "triton") )
+  
+  }
+  
+  test("planets1") {
+    val tx1 = for {
+      _ <- path("planets" :: "planet" :: "satellites" :: "satellite" :: "name" :: Nil, updateText { x => x.charAt(0).toUpper + x.substring(1) })
+      satellites <- path("planets" :: "planet" :: "satellites" :: "satellite" :: "name" :: Nil, text)
+    } yield satellites 
+  
+    assert( tx1.run(load("planets.xml"))._2.toSet == Set("Moon", "Phobos", "Deimos", "Iii ganymede", "Iv callisto", "Titan", "Rhea", "Ariel", "Miranda", "Triton") )
+  }
+  
+  test("planets2") {
+    val tx = for {
+      yellowPlanets <- selectPath(ByName("planets") :: (ByName("planet") and ByAttribute("color", _ == "yellow")) :: Nil, for {
+        nm <- selectChild("name", text)        
+        lengthOfYear <- selectChild("lengthOfYear", text)        
+      } yield  { 
+        <planet name={nm.map(xml.Text(_))} year={lengthOfYear.map(xml.Text(_))} /> 
+      })
+      _ <- write( <yellowPlanets>{yellowPlanets}</yellowPlanets> )
+    } yield <yellow>{yellowPlanets}</yellow>
+    
+    println( tx.run(load("planets.xml"))._2 )
+    println( tx.run(load("planets.xml"))._1 )
+    
+  }
+  
+  
 }
